c87a0f0493c0869af8a109a0547be0aa1913eae7
chandra kumar kusam <chandrakumarkusam@chandras-MacBook-Air.local>
2025-01-25 16:43:00 +0530

    search feature added

--- src/components/NavBarComponent.jsx
+++ src/components/NavBarComponent.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Logout from "./Logout";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = styled.div`
   display: flex;
@@ -13,22 +15,103 @@ const Navbar = styled.div`
 `;
 
 const SearchInput = styled.input`
-  width: 50%;
+  width: 100%;
   padding: 8px 12px;
   border: none;
   border-radius: 8px;
   font-size: 16px;
 `;
 
-const LogoutButton = styled.button`
-  
-`;
+const SearchContainer = styled.div`
+  position: relative;
+  width: 50%;
+  padding: 8px 12px;
+  border: none;
+  border-radius: 8px;
+  font-size: 16px;
+`
+
+const ResultContainer = styled.div`
+  position: absolute;
+  background-color: white;
+  color: black;
+  width: 100%;
+  /* padding: 12px; */
+  font-size: 16px;
+`
+
+const Noresults = styled.div`
+  position: absolute;
+  background-color: white;
+  color: red;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 16px;
+  padding-top: 8px;
+  padding-bottom: 8px;
+`
+
+const ResultDiv = styled.div`
+  padding-top: 1px;
+  padding-bottom: 1px;
+  cursor: pointer;
+  border-bottom-left-radius: 8px;
+  border-bottom-right-radius: 8px;
+  &:hover {
+    background-color: rgba(157, 150, 150, 0.1);
+  }
+`
 
 const NavbarComponent = () => {
+    const [results, setResults] = useState([])
+    const [opened, setOpened] = useState(false)
+    const navigate = useNavigate()
+    const handleChange = async (e) => {
+      if(e.target.value.length >= 1) 
+        setOpened(true)
+      else 
+        setOpened(false)
+      try {
+        let res = await axios.get(`http://localhost:8000/expense/search?keyword=${e.target.value}`, {
+          withCredentials: true
+        })
+        if(res.status === 200) {
+          console.log(res.data);
+          
+          setResults(res.data)
+        }
+      } catch (error) {
+        console.log("error in fetching results", error);
+        
+      }
+    }
+
+    const handleBlur = () => {
+      setTimeout(() => setOpened(false), 200);
+    };
+
   return (
     <Navbar>
       <div></div>
-      <SearchInput type="text" placeholder="Search..." />
+      <SearchContainer onBlur={handleBlur} >
+          <SearchInput onChange={handleChange} type="text" placeholder="Search..." />
+          {opened && (results.length !== 0?
+          <ResultContainer>
+            {results.length !== 0 && results.map((exp, ind) => {
+              return <ResultDiv key={ind} onMouseDown={(e) => {
+                e.preventDefault();
+                navigate(`/expense/${exp.id}`);
+              }} >
+                <p style={{margin:"20px"}} > Title:- {exp.title}</p>
+                <p style={{margin:"20px"}} >Category:- {exp.category}</p>
+              </ResultDiv>
+            })}
+          </ResultContainer>
+          :<Noresults>No Expenses Found</Noresults>)}
+      </SearchContainer>
+      
       <Logout/>
     </Navbar>
   );
